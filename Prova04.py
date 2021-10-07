7d9aba4534ff67acbb0cca34d2035531551d742e
Alex Rodrigues <88850921+Alexkakakaka@users.noreply.github.com>
2021-10-07 16:15:20 -0300

    Update Prova04.py

--- Prova04.py
+++ Prova04.py
@@ -4,5 +4,5 @@ valornovo = valor - imposto
 primeiro= valornovo * 46/100
 segundo = valornovo * 32/100
 terceiro = valornovo - (primeiro + segundo)
-print("o Premio doi de r$ {}, o valor desconto ficou R$ {} o imposto ficou R$ {}".format(valor, valornovo, imposto))
+print("o Premio foi de r$ {}, o valor desconto ficou R$ {} o imposto ficou R$ {}".format(valor, valornovo, imposto))
 print("O primeiro ganhador ganhou R$ {}, o segundo ganhaou R$ {} eo terceiro ganhador ganhou R$ {}".format(primeiro, segundo, terceiro))
